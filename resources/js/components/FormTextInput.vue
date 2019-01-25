d98e6f1c5151dd0239b56ebb166f3942ff47d228
GabrielMioni <gabriel.mioni@gmail.com>
2019-01-24 21:49:18 -0800

    Controlled component to render label and text/textarea inputs

---
+++ resources/js/components/FormTextInput.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="form-group" v-bind:class="{ 'form-inline inline-input no-gutters' : isInline }">
+        <div v-if="isInline" class="col-sm-2">
+            <label class="justify-content-start" :for="setInputName(inputTitle)">{{ inputTitle | ucFirst }}</label>
+        </div>
+        <label v-else :for="setInputName(inputTitle)">{{ inputTitle | ucFirst }}</label>
+        <input v-if="isTextArea === false"
+               type="text" class="form-control"
+               v-bind:class="{'col-sm-10 pl-3' : isInline }"
+               :name="setInputName(inputTitle)"
+               :value="value"
+               v-on:input="updateInput"
+        >
+        <textarea v-else
+                  class="form-control"
+                  v-bind:class="{'col-sm-10 pl-3' : isInline }"
+                  :name="setInputName(inputTitle)"
+                  :value="value"
+                  v-on:input="updateInput"
+        ></textarea>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "form-text-input",
+        props: {
+            inputTitle: {
+                type: String,
+                required: true
+            },
+            setInputName: {
+                type: Function,
+                required: true
+            },
+            value: {
+                type: String,
+                required: true
+            },
+            isTextArea: {
+                type: Boolean,
+                require: false,
+                default: false,
+            },
+            isInline: {
+                type: Boolean,
+                require: false,
+                default: false,
+            },
+        },
+        methods: {
+            updateInput(e) {
+                let text = e.target.value;
+                this.$emit('input', text);
+            },
+        },
+        filters: {
+            ucFirst(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
+        }
+    }
+</script>
