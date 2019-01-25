ea5601c61a48e4ec8d7aaba54ef0e28790617f97
GabrielMioni <gabriel.mioni@gmail.com>
2019-01-24 21:51:43 -0800

    Admin form now uses the 'form-text-input' Vue component

--- resources/js/components/ProjectInput.vue
+++ resources/js/components/ProjectInput.vue
@@ -1,30 +1,56 @@
 <template>
-    <div class="col-sm-12 project card p-3 mb-3" v-bind:class="{reverse : checkOddEven() }">
-        <div class="row">
+    <!--<div class="col-sm-12 project card p-3 mb-3" v-bind:class="{reverse : checkOddEven() }">-->
+    <div class="col-sm-12 project card p-3 mb-3">
+        <div class="form-row">
             <div class="col-sm-6 image-holder">
                 <div class="project-image form-control" v-bind:style="{ backgroundImage: 'url(' + setUrl(project['image_main'], project['image_main_ext']) + ')' }"></div>
             </div>
             <div class="col-sm-6 text-holder">
                 <div class="admin-edit">
-                    <label :for="setInputName('title')">Title</label>
-                    <input type="text" class="form-control"
-                           :name="setInputName('title')"
-                           v-model="project.title"
-                    >
-                    <label :for="setInputName('description')">Description</label>
-                    <textarea class="form-control"
-                              :name="setInputName('description')"
-                              v-model="project.description"
-                    ></textarea>
+                    <form-text-input
+                            :inputTitle="'title'"
+                            v-model="project.title"
+                            :setInputName="setInputName"
+                    ></form-text-input>
+                    <form-text-input
+                            :inputTitle="'description'"
+                            v-model="project.description"
+                            :setInputName="setInputName"
+                            :isTextArea="true"
+                    ></form-text-input>
                 </div>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="col-sm-12">
+                <form-text-input
+                        :inputTitle="'github'"
+                        v-model="project.github"
+                        :setInputName="setInputName"
+                        :isInline="true"
+                ></form-text-input>
+                <form-text-input
+                        :inputTitle="'docs'"
+                        v-model="project.documentation"
+                        :setInputName="setInputName"
+                        :isInline="true"
+                ></form-text-input>
+                <form-text-input
+                        :inputTitle="'wordpress'"
+                        v-model="project.wordpress"
+                        :setInputName="setInputName"
+                        :isInline="true"
+                ></form-text-input>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import FormTextInput from "./FormTextInput";
     export default {
         name: "project-input",
+        components: {FormTextInput},
         props: ['project', 'index'],
         methods: {
             setUrl(file, ext) {
