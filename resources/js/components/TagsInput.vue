6f46424db5f9fc5f44e734d6a8ea90f15cb3f44e
GabrielMioni <gabriel.mioni@gmail.com>
2019-02-10 09:05:50 -0800

    Scaffolding for tag input

---
+++ resources/js/components/TagsInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="form-group" v-bind:class="{ 'form-inline inline-input no-gutters' : isInline }">
+        <label class="justify-content-start">Tags</label>
+        <div class="tag-area">
+            <div class="tag" v-for="tag in tags">{{tag}}</div>
+        </div>
+        <input v-model="tagInput" >
+        <div class="tag-suggestions">
+            <div class="tag" v-for="tag in tags">{{tag}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        model: {
+            prop: "tags",
+            event: "addTag",
+        },
+        name: "tags-input",
+        props: ['tags', 'availableTags'],
+        data() {
+            return {
+                tagInput : '',
+            };
+        },
+        methods: {
+            toggle() {
+                //this.$emit("addTag", !this.toggled)
+                this.$emit("addTag", false)
+            }
+        },
+    }
+</script>
