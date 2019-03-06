68b4422e30e2378f781ffec3a0f7e4a8f9711b89
GabrielMioni <gabriel.mioni@gmail.com>
2019-03-05 22:00:09 -0800

    Update project and tag data

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    protected $existingTags = null;
+
     public function index() {
         return view('admin');
     }
@@ -15,7 +17,7 @@ class AdminController extends Controller
     public function getProjects() {
 
         $projects = Project::select('id','title','description','github','wordpress','documentation','image_main','image_main_ext', 'order_column')
-                                       ->with(array('tags' => function($q) { $q->select('tag'); }))->get();
+                                       ->with(array('tags' => function($q) { $q->select('tag'); }))->orderBy('order_column', 'asc')->get();
 
         $projectData = [];
 
@@ -34,6 +36,80 @@ class AdminController extends Controller
         }
 
         return $projectData;
+    }
+
+    public function updateProject(Request $request) {
+        $projects = json_decode($request->get('projects'), true);
+        $files    = $request->file('file');
+
+        foreach ($projects as $key => $projectData) {
+            $id = $projectData['id'];
+
+            $project = Project::find($id);
+
+            foreach ($projectData as $innerKey => $value) {
+                if ($innerKey === 'id') {
+                    continue;
+                }
+                if ($innerKey === 'tags') {
+                    $this->processTags($value, $project);
+                }
+                if ($project->$innerKey !== $value) {
+                    $project->$innerKey = $value;
+                }
+                $project->save();
+
+            }
+        }
+    }
+
+    protected function getProjectTags(Project $project) {
+
+        $out = [];
+
+        $tags = $project->Tags()->get()->toArray();
+
+        foreach ($tags as $tagData) {
+            $tagId = $tagData['id'];
+            $out[$tagId] = $tagData['tag'];
+        }
+
+        return $out;
+    }
+
+    protected function processTags(array $tags, Project $project) {
+
+        if ($this->existingTags === null) {
+            $this->existingTags = $this->allTags();
+        }
+
+        $storedTags = $this->getProjectTags($project);
+
+        // Remove absent tags
+        foreach ($storedTags as $tagId => $tagValue) {
+            if (!in_array($tagValue, $tags)) {
+                $project->tags()->detach($tagId);
+            }
+        }
+
+        // Add new tags
+        foreach ($tags as $tag) {
+            $tagId = null;
+            $tag = trim($tag);
+
+            if (!in_array($tag, $this->existingTags)) {
+                $newTag = new Tag();
+                $newTag->tag = $tag;
+                $newTag->save();
+                $tagId = $newTag->id;
+            }
+            else {
+                $tagId = Tag::where('tag', $tag)->first()->id;
+            }
+            if ($tagId !== null && !$project->tags->contains($tagId)) {
+                $project->tags()->attach($tagId);
+            }
+        }
     }
 
     public function allTags() {
