c94cbee72e2059d450a05b056535e2258b233925
GabrielMioni <gabriel.mioni@gmail.com>
2019-03-30 14:51:21 -0700

    Hero cta 'buttons'

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -8,6 +8,14 @@
                 Web Developer<br>
                 Available for hire
             </h2>
+            <div class="row">
+                <div class="col-sm-6">
+                    <a href="">My Projects</a>
+                </div>
+                <div class="col-sm-6">
+                    <a href="">Contact Me</a>
+                </div>
+            </div>
         </div>
     </div>
     <div>I am the front-end place</div>
