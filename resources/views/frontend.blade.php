34ec67446d84d148252838619b6808f9ce7093f4
GabrielMioni <gabriel.mioni@gmail.com>
2019-03-30 14:21:22 -0700

    Hero image templating

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -2,5 +2,13 @@
 @include('layouts.nav-frontend')
 
 @section('content')
+    <div class="container-fluid front-end-hero">
+        <div class="hero-cta">
+            <h2>
+                Web Developer<br>
+                Available for hire
+            </h2>
+        </div>
+    </div>
     <div>I am the front-end place</div>
 @endsection
