653413eae2f893afb66363ca9ca49f87c8a89387
GabrielMioni <gabriel.mioni@gmail.com>
2019-03-30 17:38:52 -0700

    FrontEndProject gets all tags when mounted

--- resources/js/components/FrontEndProjects.vue
+++ resources/js/components/FrontEndProjects.vue
@@ -5,7 +5,30 @@
 </template>
 
 <script>
+    import { callAxios } from '../call-axios'
+
     export default {
-        name: "front-end-projects"
+        name: "front-end-projects",
+        data() {
+            return {
+                allTags: [],
+                projects: [],
+            }
+        },
+        methods: {
+            getTags() {
+                const self = this;
+                callAxios(self.$options.tags_url, (data_obj) => {
+                    self.allTags = data_obj;
+                });
+            },
+        },
+        created() {
+            this.$options.projects_url = '/projects-json';
+            this.$options.tags_url = '/all-tags';
+        },
+        mounted() {
+            this.getTags();
+        }
     }
 </script>
