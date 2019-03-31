d54243de95cf725dd0043df28712b9055d6920d8
GabrielMioni <gabriel.mioni@gmail.com>
2019-03-30 19:12:29 -0700

    Component with tag checkboxes for controlling filtered projects

--- resources/js/components/ProjectFilter.vue
+++ resources/js/components/ProjectFilter.vue
@@ -1,11 +1,31 @@
 <template>
-
+    <div class="project-filter">
+        <h3>Filter Projects</h3>
+        <div class="project-tags">
+            <ul>
+                <li v-for="(tag, index) in allTags" :key="index">
+                    <label><input
+                        @change="checkboxUpdate"
+                        type="checkbox"
+                        :name="tag"
+                    />{{tag}}</label>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
-
     export default {
         name: "ProjectFilter",
         props: ['allTags'],
+        methods: {
+            checkboxUpdate(e) {
+                const checkbox = e.target;
+                const isChecked = checkbox.checked;
+                const name = checkbox.name;
+                this.$emit("updateFilter", {'tagName': name, 'value' : isChecked});
+            }
+        }
     }
 </script>
