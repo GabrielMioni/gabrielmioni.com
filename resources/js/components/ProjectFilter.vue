681d84bf54ec41898d718f662bc1d79c0d41eb35
GabrielMioni <gabriel.mioni@gmail.com>
2019-03-31 16:27:09 -0700

    Make ProjectFilter component expandable

--- resources/js/components/ProjectFilter.vue
+++ resources/js/components/ProjectFilter.vue
@@ -1,16 +1,24 @@
 <template>
-    <div class="project-filter">
-        <h3>Filter Projects</h3>
-        <div class="project-tags">
-            <ul>
-                <li v-for="(tag, index) in allTags" :key="index">
-                    <label><input
-                        @change="checkboxUpdate"
-                        type="checkbox"
-                        :name="tag"
-                    />{{tag}}</label>
-                </li>
-            </ul>
+    <div class="project-filter mb-5" v-bind:class="{'expanded' : expanded}">
+        <div class="col-sm-12">
+            <div class="project-filter-inner">
+                <div class="filter-control"
+                    @click="toggleExpanded">
+                    <h5>Filter Projects</h5>
+                    <i class="fas fa-sort-down"></i>
+                </div>
+                <div class="project-tags">
+                    <ul>
+                        <li v-for="(tag, index) in allTags" :key="index">
+                            <label><input
+                                @change="checkboxUpdate"
+                                type="checkbox"
+                                :name="tag"
+                            />{{tag}}</label>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -19,7 +27,15 @@
     export default {
         name: "ProjectFilter",
         props: ['allTags'],
+        data() {
+            return {
+                expanded: false,
+            }
+        },
         methods: {
+            toggleExpanded() {
+                this.expanded = !this.expanded;
+            },
             checkboxUpdate(e) {
                 const checkbox = e.target;
                 const isChecked = checkbox.checked;
