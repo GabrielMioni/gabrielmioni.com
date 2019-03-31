a26529c993cbbeedea021cb7d8292ee483af1abc
GabrielMioni <gabriel.mioni@gmail.com>
2019-03-31 15:51:05 -0700

    Cleanup

--- resources/js/components/ProjectDisplay.vue
+++ resources/js/components/ProjectDisplay.vue
@@ -5,28 +5,15 @@
                 <div class="project-image-wrap">
                     <img v-bind:src="setUrl()">
                 </div>
-                <!--<div class="project-image" v-bind:style="{ backgroundImage: 'url(' + setUrl() + ')' }"></div>-->
             </div>
             <div class="col-md-6">
-                <h2>{{ project.title }}</h2>
+                <h2 class="mb-3">{{ project.title }}</h2>
                 <div class="project-description">
                     {{ project.description}}
                 </div>
-                <!--ID: {{project.id}} <br>
-                Order: {{project.order_column}} <br>-->
-                <!--Image: {{project.image_main}}.{{project.image_main_ext}} <br>-->
-                <!--Description: {{project.description}} <br>-->
-                <!--<div v-if="checkLinks()" class="project-links row justify-content-start">
-                    <div v-if="checkLinks('github')" class="col-sm-3">
-                        <a v-bind:href="project.github">Github</a>
-                    </div>
-                    <div v-if="checkLinks('wordpress')" class="col-sm-3">
-                        <a v-bind:href="project.wordpress">Wordpress</a>
-                    </div>
-                </div>-->
-                <div v-if="checkLinks()" class="project-links">
-                    <a v-if="checkLinks('github')" v-bind:href="project.github">Github</a>
-                    <a v-if="checkLinks('wordpress')" v-bind:href="project.wordpress">Wordpress</a>
+                <div v-if="checkLinks()" class="project-links mt-3">
+                    <a v-if="checkLinks('github')" v-bind:href="project.github"><i class="fab fa-github"></i>Github</a>
+                    <a v-if="checkLinks('wordpress')" v-bind:href="project.wordpress"><i class="fab fa-wordpress-simple"></i>Wordpress</a>
                 </div>
             </div>
         </div>
