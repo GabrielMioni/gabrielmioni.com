00be730e66930b5b5bc7ca84767ff8026a93aa80
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-01 20:18:38 -0700

    Moved all-tags route back to middleware. Added route for getting only attached tags

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@
 
 Route::get('/', 'FrontendController@index')->name('frontend');
 Route::get('/projects-json', 'AdminController@getProjects')->name('projects_json');
-Route::get('/all-tags', 'AdminController@allTags')->name('all_tags');
+Route::get('/attached-tags', 'AdminController@attachedTags')->name('attached-tags');
 
 Auth::routes();
 
@@ -25,6 +25,7 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function() {
     Route::get('/admin', 'AdminController@index')->name('admin');
     Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/all-tags', 'AdminController@allTags')->name('all_tags');
     Route::post('/project-store', 'AdminController@storeProject')->name('store_project');
     Route::post('/project-store-sort-order', 'AdminController@storeNewSortOrder')->name('store_sort_order');
     Route::post('/project-delete', 'AdminController@removeProject')->name('delete_project');
