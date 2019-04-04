da7acd0e2da4658790a611fb3d8d0d7818432199
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-03 19:47:03 -0700

    Use babel for IE11 compatability

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,4 +11,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+   .babel('public/js/app.js', 'public/js/app.js')
    .sass('resources/sass/app.scss', 'public/css');
