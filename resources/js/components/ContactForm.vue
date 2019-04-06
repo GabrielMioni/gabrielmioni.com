4e1ac45594aabcc617683dce058b9872b8f9945b
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-05 17:34:11 -0700

    Contact Form validation

--- resources/js/components/ContactForm.vue
+++ resources/js/components/ContactForm.vue
@@ -1,28 +1,80 @@
 <template>
-    <form>
+    <form class="contact-form">
         <h2>Contact me</h2>
         <div class="form-group">
-            <label for="name">Your Name *</label>
-            <input type="text" class="form-control" id="name">
+            <label for="name">
+                <span>Your Name *</span>
+                <span v-if="errors.message.length > 0" class="error-message">{{errors.message}}</span>
+            </label>
+            <input v-model="name" type="text" class="form-control" id="name">
         </div>
         <div class="form-group">
-            <label for="email">Your Email *</label>
-            <input type="email" class="form-control" id="email">
+            <label for="name">
+                <span>Your Email *</span>
+                <span v-if="errors.email.length > 0" class="error-message">{{errors.email}}</span>
+            </label>
+            <input v-model="email" type="email" class="form-control" id="email">
         </div>
         <div class="form-group">
-            <label for="company">Company</label>
-            <input type="text" class="form-control" id="company">
+            <label for="company">
+                <span>Company</span>
+            </label>
+            <input v-model="company" type="text" class="form-control" id="company">
         </div>
         <div class="form-group">
-            <label for="message">Message *</label>
-            <textarea class="form-control" id="message" name="message"></textarea>
+            <label for="message">
+                <span>Message *</span>
+                <span v-if="errors.message.length > 0" class="error-message">{{errors.message}}</span>
+            </label>
+            <textarea v-model="message" class="form-control" id="message" name="message"></textarea>
         </div>
-        <button type="button" class="btn btn-control"></button>
+        <button @click="submitEmail" type="button" class="btn btn-cta">Submit</button>
     </form>
 </template>
 
 <script>
+    import { callAxios } from '../call-axios';
+
     export default {
-        name: "ContactForm"
+        name: "ContactForm",
+        data() {
+            return {
+                name: '',
+                email: '',
+                company: '',
+                message: '',
+                errors: {
+                    name: '',
+                    email: '',
+                    message: '',
+                },
+            }
+        },
+        methods: {
+            validateEmail(message) {
+                const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                const check = re.test(String(this.email).toLowerCase());
+                if (check === false) {
+                    this.errors.email = message;
+                }
+                return check;
+            },
+            checkEmpty(type, message) {
+                const check = this[type].trim().length > 0;
+
+                if (check === false) {
+                    this.errors[type] = message;
+                }
+            },
+            submitEmail() {
+                const checkEmail = this.validateEmail('Please provide a valid email address');
+                const checkName  = this.checkEmpty('name', 'Please include your name');
+                const checkMessage = this.checkEmpty('message', 'Please include a message');
+
+                if (checkEmail === false || checkName === false || checkMessage === false) {
+                    return;
+                }
+            }
+        }
     }
 </script>
