017edca0a5b8d0b9c1d26696ed70550cb92cb7b8
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-07 16:39:30 -0700

    Moved ContactForm into ContactMe. Added ContactModal

---
+++ resources/js/components/ContactModal.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <div class="message-complete">{{ message }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ContactModal",
+        props: ['title', 'message']
+    }
+</script>
