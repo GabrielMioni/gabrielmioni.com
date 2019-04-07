38c5cb94ac56ad83e29e6ba2f2c6b3e3e9e35721
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-07 16:38:43 -0700

    Email send to using .env value

--- resources/views/email/contact.blade.php
+++
@@ -1,7 +0,0 @@
-@component('mail::message')
-    <div>
-        <div>Name: {{ $contactName }}</div>
-        <div>Email: {{ $contactEmail }}</div>
-        <div>Company: {{ $contactCompany }}</div>
-    </div>
-@endcomponent
