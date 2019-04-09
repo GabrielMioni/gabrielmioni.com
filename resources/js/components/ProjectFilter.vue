a2086a206e0782bbbdf584bd08825757d6e68da9
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-08 18:27:04 -0700

    Cleaned up some grid items

--- resources/js/components/ProjectFilter.vue
+++ resources/js/components/ProjectFilter.vue
@@ -1,23 +1,21 @@
 <template>
     <div class="project-filter mb-5" v-bind:class="{'expanded' : expanded}">
-        <div class="col-sm-12">
-            <div class="project-filter-inner">
-                <div class="filter-control"
-                    @click="toggleExpanded">
-                    <h5>Filter Projects</h5>
-                    <i class="fas fa-sort-down"></i>
-                </div>
-                <div class="project-tags">
-                    <ul>
-                        <li v-for="(tag, index) in allTags" :key="index">
-                            <label><input
-                                @change="checkboxUpdate"
-                                type="checkbox"
-                                :name="tag"
-                            />{{tag}}</label>
-                        </li>
-                    </ul>
-                </div>
+        <div class="project-filter-inner">
+            <div class="filter-control"
+                 @click="toggleExpanded">
+                <h5>Filter Projects</h5>
+                <i class="fas fa-sort-down"></i>
+            </div>
+            <div class="project-tags">
+                <ul>
+                    <li v-for="(tag, index) in allTags" :key="index">
+                        <label><input
+                            @change="checkboxUpdate"
+                            type="checkbox"
+                            :name="tag"
+                        />{{tag}}</label>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
