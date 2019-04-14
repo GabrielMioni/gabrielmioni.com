8257e5c13736a7e705b14c185c04d72bdb3bfbf7
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-14 11:08:41 -0700

    New components

---
+++ resources/js/components/LoadingSpinner.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-if="loading" class="spin-wrapper">
+        <i class="fas fa-circle-notch fa-spin"></i>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LoadingSpinner",
+        props: ['loading']
+    }
+</script>
---
+++ resources/js/components/SubmitButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button
+        @click="clickAction"
+        v-bind:class="{ 'disabled' : disabled }"
+        type="button" class="btn btn-cta">{{ buttonText | setButtonText }}</button>
+</template>
+
+<script>
+    export default {
+        name: "SubmitButton",
+        props: ['buttonText', 'disabled'],
+        methods: {
+            clickAction() {
+                this.$emit('buttonClick');
+            }
+        },
+        filters: {
+            setButtonText(string) {
+                if (string.trim() === '' && string === null) {
+                    return 'Submit';
+                }
+                return string;
+            }
+        }
+    }
+</script>
