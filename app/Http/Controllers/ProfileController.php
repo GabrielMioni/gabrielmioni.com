fe26af7e44c656718c788385901824e3d3d75443
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-16 16:42:11 -0700

    Use saveImageTrait

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 
 class ProfileController extends Controller
 {
+    use saveImageTrait;
+
     public function index() {
         return view('profile');
     }
