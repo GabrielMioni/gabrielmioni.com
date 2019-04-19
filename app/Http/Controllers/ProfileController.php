088b0cef534ee73e4c0163c985b1bc5824a0443b
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-19 13:39:01 -0700

    Use camelCase instead of snake_case for eloquent query columns

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -11,5 +12,10 @@ class ProfileController extends Controller
 
     public function index() {
         return view('profile');
+    }
+    public function getPrivateProfileData() {
+        $publicProfileData = Profile::select('aboutMe', 'avatar', 'contactEmail', 'email', 'github', 'linkedIn', 'name', 'tagLine')->where('id', '=', 1)->first()->toArray();
+
+        return $publicProfileData;
     }
 }
