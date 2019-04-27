aef704f6aa52c959cfc7923978997fe9ff1b8b8e
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-27 12:01:45 -0700

    Endpoint for gettings tags/projects data

--- app/Http/Controllers/TagAdminController.php
+++ app/Http/Controllers/TagAdminController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Project;
+use App\Tag;
 
 class TagAdminController extends Controller
 {
     public function index() {
         return view('tag-admin');
+    }
+    public function getTagsData() {
+        $tags = Tag::select('*')->with(array('projects' => function($q) {$q->select('projects.id','title', 'description'); }))->get()->toArray();
+        //$tags = Tag::select('*')->with(array('projects' => function($q) {$q->select('*'); }))->get()->toArray();
+
+        return $tags;
     }
 }
