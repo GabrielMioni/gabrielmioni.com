a1f0a26387a8b25216a05b5447786dd8752aebd1
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-27 12:02:01 -0700

    Loop through projects

---
+++ resources/js/components/AdminTags.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div v-for="tag in tagsProjects" :key="tag.id" class="tag-row">
+            {{tag.tag}}
+        </div>
+    </div>
+</template>
+
+<script>
+    import { callAxios } from '../call-axios'
+
+    export default {
+        name: "AdminTags",
+        data() {
+            return {
+                tagsProjects: [],
+            }
+        },
+        methods: {
+            getTagsProjectsData() {
+                callAxios(this.$options.tagsProjectsData, (dataObj) => {
+                    console.log(dataObj);
+                    this.tagsProjects = dataObj;
+                });
+            }
+        },
+        mounted() {
+            this.getTagsProjectsData();
+        },
+        created() {
+            this.$options.tagsProjectsData = '/tags-projects';
+        }
+    }
+</script>
--- resources/views/tag-admin.blade.php
+++ resources/views/tag-admin.blade.php
@@ -4,7 +4,7 @@
 	<div class="container py-4">
 		<div class="row justify-content-center">
 			<div class="col-md-12">
-				The spoiler is Robert Downy Jr. don't wanna.
+				<admin-tags></admin-tags>
 			</div>
 		</div>
 	</div>
