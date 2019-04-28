036998cf8491e955fd9836bb9f0264650858075d
GabrielMioni <gabriel.mioni@gmail.com>
2019-04-28 12:50:58 -0700

    Structure/styles for displaying projects related to tags

--- resources/js/components/AdminTags.vue
+++ resources/js/components/AdminTags.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="col-sm-12">
-        <table class="table table-striped">
+        <table class="table">
             <thead>
                 <tr>
                     <th>Tag Name</th>
@@ -9,11 +9,48 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="tag in tagsProjects" :key="tag.id" class="tag-row">
-                    <td class="tag-name">{{tag.tag}}</td>
-                    <td class="tag-created">{{tag.created | dateFormat }}</td>
-                    <td></td>
-                </tr>
+                <template v-for="(tag, tagIndex) in tagsProjects">
+                    <tr :key="tag.id">
+                        <td>
+                            <input
+                                v-model="tagsProjects[tagIndex].tag"
+                                v-bind:name="'tag-name-'+tag.id"
+                                type="text" class="form-control">
+                        </td>
+                        <td class="tag-created align-middle">{{tag.created | dateFormat }}</td>
+                        <td></td>
+                    </tr>
+                    <tr
+                        v-if="tag.projects.length > 0"
+                        class="projects-row">
+                        <td class="project-td-container" colspan="3">
+                            <div class="project-table-container">
+                                <table class="table">
+                                    <thead class="thead-light">
+                                    <tr>
+                                        <th>Project</th>
+                                        <th>Description</th>
+                                        <th></th>
+                                    </tr>
+                                    </thead>
+                                    <tr
+                                        v-for="(project, projectIndex) in tag.projects">
+                                        <td>{{ project.title }}</td>
+                                        <td>{{ project.description }}</td>
+                                        <td></td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <!--<div
+                                v-for="(project, projectIndex) in tag.projects"
+                                class="row">
+                                <div class="col-sm-12">
+                                    {{project.title}}
+                                </div>
+                            </div>-->
+                        </td>
+                    </tr>
+                </template>
             </tbody>
         </table>
     </div>
@@ -40,7 +77,7 @@
         },
         filters: {
             dateFormat: function (date) {
-                return moment(date).format('MMMM Do YYYY, H:mm a');
+                return moment(date).format('MMMM Do YYYY, h:mm a');
             }
         },
         mounted() {
