d05232a85885c2b722e3b1174fc85d1c84e7aed3
GabrielMioni <gabriel.mioni@gmail.com>
2019-05-27 11:48:45 -0700

    oooh he's tryin

--- resources/js/call-axios.js
+++ resources/js/call-axios.js
@@ -2,12 +2,14 @@ module.exports = {
   callAxios: (url, callback) => {
     axios.get(url)
       .then((data) => {
+        console.log('data', data);
         const data_obj = data.data;
         if (typeof callback === 'function') {
           callback(data_obj)
         }
       })
       .catch((error)=>{
+        console.log('error', error);
         callback(error);
       })
   },
